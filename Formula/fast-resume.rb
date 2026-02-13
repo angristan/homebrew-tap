99c637d2344cfc291702ae8b26c3a411bc25e9f8
Stanislas Lange <git@slange.me>
2026-02-13 19:06:10 +0100

    Add fast-resume formula

---
+++ Formula/fast-resume.rb
@@ -0,0 +1,29 @@
+class FastResume < Formula
+  desc "Fuzzy finder for coding agent session history"
+  homepage "https://github.com/angristan/fast-resume"
+  version "1.16.2"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/angristan/fast-resume/releases/download/v#{version}/fast-resume-#{version}-macos-arm64.tar.gz"
+      sha256 "5a85616c00f262caf7e380a35dbde892d5a37b6f51fc1da992f87d7195511ef7"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/angristan/fast-resume/releases/download/v#{version}/fast-resume-#{version}-linux-x86_64.tar.gz"
+      sha256 "ccc31e35d74d28b9d51a8bbc324540adc32eb551a7929b65aa7564db3f901881"
+    end
+  end
+
+  def install
+    bin.install "fr"
+    bin.install_symlink "fr" => "fast-resume"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/fr --version")
+  end
+end

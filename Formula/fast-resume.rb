4d134b8265e349c1692e831383e117f80e8f9a99
Thiago Perrotta <thiago@perrotta.dev>
2026-05-26 12:43:24 +0200

    fast-resume 1.17.2

--- Formula/fast-resume.rb
+++ Formula/fast-resume.rb
@@ -1,20 +1,20 @@
 class FastResume < Formula
   desc "Fuzzy finder for coding agent session history"
   homepage "https://github.com/angristan/fast-resume"
-  version "1.16.2"
+  version "1.17.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/angristan/fast-resume/releases/download/v#{version}/fast-resume-#{version}-macos-arm64.tar.gz"
-      sha256 "5a85616c00f262caf7e380a35dbde892d5a37b6f51fc1da992f87d7195511ef7"
+      sha256 "96d920a9b413b1f0026be87319134f687e0dc9ab5b707c71c1d99dec901c3b52"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/angristan/fast-resume/releases/download/v#{version}/fast-resume-#{version}-linux-x86_64.tar.gz"
-      sha256 "ccc31e35d74d28b9d51a8bbc324540adc32eb551a7929b65aa7564db3f901881"
+      sha256 "a0166534eb698cdda1f2f4600cd4183ae03f0830eb9bed10899edd286d44b548"
     end
   end
 

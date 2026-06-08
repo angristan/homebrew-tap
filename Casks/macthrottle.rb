7e879dc34d7d5684b562d74fab5777964a442727
Qi Zhang <singularitti@outlook.com>
2026-06-08 06:46:29 -0500

    Fix "Calling string comparison format for `depends_on macos:` is deprecated"

--- Casks/macthrottle.rb
+++ Casks/macthrottle.rb
@@ -7,7 +7,7 @@ cask "macthrottle" do
   desc "Menu bar app that monitors thermal throttling"
   homepage "https://github.com/angristan/MacThrottle"
 
-  depends_on macos: ">= :sequoia"
+  depends_on macos: :sequoia
 
   preflight do
     system_command "/usr/bin/xattr",
